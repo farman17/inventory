847231f77172bca56440e1d8548b5986aa6c8372
farman17 <ahmadfarman1701@gmail.com>
2020-12-21 23:54:18 +0000

    ubah dbconnect database

--- dbconnect.php
+++ dbconnect.php
@@ -2,9 +2,9 @@
 
 /* Dibuat Oleh D_ziem */
 
-  $DB_host = "localhost";
-  $DB_user = "root";
-  $DB_pass = "";
+  $DB_host = "dbsosmedia.cjorhe5jzkni.us-east-2.rds.amazonaws.com";
+  $DB_user = "admin";
+  $DB_pass = "farman17";
   $DB_name = "stok_barang";
   
   $MySQLi_CON = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
